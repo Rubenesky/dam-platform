c3a8dd7fe5f729c8f0334801817b37110aee14be
Ruben <dcrubben25@gmail.com>
2026-03-30 11:03:54 +0200

    feat: add dashboard with charts and activity feed

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])
+     ->middleware(['auth', 'verified'])
+     ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     // Rutas de perfil
